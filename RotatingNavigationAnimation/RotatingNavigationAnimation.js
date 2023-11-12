3dd81c11ea95470726bd41b16b36cff1ca3da745
NathnaelTB <natibekaffa12@gmail.com>
2023-11-12 22:54:04 +0300

    add the nav bar

--- RotatingNavigationAnimation/RotatingNavigationAnimation.js
+++ RotatingNavigationAnimation/RotatingNavigationAnimation.js
@@ -1,12 +1,15 @@
 const body = document.getElementsByTagName('body')[0];
 const corner = document.getElementsByClassName('corner')[0];
+const nav = document.getElementsByClassName('nav')[0];
 
 const getOption = () => {
     body.classList.add('rotator');
     corner.classList.add('option-rotator');
+    nav.classList.add('nav-unrotate');
 }
 
 const cancel = () => {
     body.classList.remove('rotator');
     corner.classList.remove('option-rotator');
+    nav.classList.remove('nav-unrotate');
 }
