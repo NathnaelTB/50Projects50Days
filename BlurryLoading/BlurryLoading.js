43773e1d04f0d19060abf5c09f1fdaaf381408a4
NathnaelTB <natibekaffa12@gmail.com>
2023-11-14 12:05:20 +0300

    complete BlurryLoading

--- BlurryLoading/BlurryLoading.js
+++ BlurryLoading/BlurryLoading.js
@@ -1,5 +1,19 @@
 const picture = document.getElementsByClassName("picture")[0];
+const percentage = document.getElementsByClassName("percentage")[0];
+
+let num = 0;
 
 const unblur = () => {
     picture.classList.add('unblur');
+    percentage.classList.add('blurPercentage');
+    setInterval(loader, 25);
+}
+
+function loader() {
+    if(num === 100){
+        clearInterval();
+        return;
+    }
+    num += 1;
+    percentage.innerHTML = num + '%';
 }
