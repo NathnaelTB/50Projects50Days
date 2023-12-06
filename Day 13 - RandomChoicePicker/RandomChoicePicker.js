effe3428d57c7ac983517f4818868208cd60a007
NathnaelTB <natibekaffa12@gmail.com>
2023-12-06 17:47:15 +0300

    add shuffle effect (Day 13 - RandomChoicePicker)

--- Day 13 - RandomChoicePicker/RandomChoicePicker.js
+++ Day 13 - RandomChoicePicker/RandomChoicePicker.js
@@ -9,24 +9,43 @@ function displayText(event) {
     const word = event.target.value.split(',');
 
     if(event.keyCode === 13) {
+        shuffle(word);
         chooseRandomly(word);
     }
     
     else {
         let wordHTML = '';
         word.forEach((item, index) => {
-            wordHTML += `<h3 id=${index}>` + item  + `</h3>`;
+            wordHTML += `<h3 class=${index}>` + item  + `</h3>`;
         })
         choices.innerHTML = wordHTML;
     }
 }
 
 function chooseRandomly(word) {
-    // shuffle(word);
-    let randomIndex = Math.floor(Math.random() * word.length) // generates integer between 0 and word.length
-    choices.childNodes[randomIndex].classList.add('chosen');
+    setTimeout(() => {
+        let randomIndex = Math.floor(Math.random() * word.length) // generates integer between 0 and word.length
+        choices.childNodes[randomIndex].classList.add('chosen');
+    }, 2000);
+    
 }
 
-// function shuffle(word) {
+//shuffles choice list using random numbers
+function removeChosen(index) {
+    setTimeout(() => {
+        choices.childNodes[index].classList.remove('chosen');
+    }, 100);
+ }
 
-//  }
+function shuffle(word) {
+    let count = 0;
+    const interval = setInterval(() => {
+        let randomIndex = Math.floor(Math.random() * word.length);
+        choices.childNodes[randomIndex].classList.add('chosen');
+        removeChosen(randomIndex);
+        count += 1;
+        if(count === 10)
+        clearInterval(interval);
+    }, 200);
+ }
+
