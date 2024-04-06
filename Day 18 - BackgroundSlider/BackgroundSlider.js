e4277596e7cb365c7cd0ca85b7b9729b2badaa2a
NathnaelTB <natibekaffa12@gmail.com>
2024-04-06 21:51:35 +0300

    project finished - Day 18

--- Day 18 - BackgroundSlider/BackgroundSlider.js
+++ Day 18 - BackgroundSlider/BackgroundSlider.js
@@ -1,17 +1,30 @@
 const links = [
-    'https://images.pexels.com/photos/417083/pexels-photo-417083.jpeg?auto=compress&cs=tinysrgb&w=600',
-    'https://images.pexels.com/photos/417083/pexels-photo-417083.jpeg?auto=compress&cs=tinysrgb&w=600',
-    'https://images.pexels.com/photos/1064535/pexels-photo-1064535.jpeg?auto=compress&cs=tinysrgb&w=600',
-    'https://images.pexels.com/photos/3955093/pexels-photo-3955093.jpeg?auto=compress&cs=tinysrgb&w=600'
+    'https://images.unsplash.com/photo-1523049673857-eb18f1d7b578?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2VudGVyfGVufDB8fDB8fHww',
+    'https://images.pexels.com/photos/70746/strawberries-red-fruit-royalty-free-70746.jpeg?auto=compress&cs=tinysrgb&w=600',
+    'https://images.unsplash.com/photo-1604803778321-0424dabac230?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2VudGVyfGVufDB8fDB8fHww',
+    'https://media.istockphoto.com/id/1203599952/photo/assortment-of-fresh-organic-fruits-and-vegetables-in-rainbow-colors.webp?b=1&s=170667a&w=0&k=20&c=92WdbVXN_471z4QQdrw5oaqTQW3pBu8atfA7MVPu7fo='
 ];
 
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
+const body = document.querySelector('body');
+let count = 0;
 
 leftArrow.addEventListener('click', () => {
-    alert('left arrow clicked');
+    console.log('left');
+    if(count == 0) 
+        count = 3;
+    else 
+        count--;
+
+    body.style = `background-image: url(${links[count]})`;
 });
 
 rightArrow.addEventListener('click', () => {
-    alert('right arrow clicked');
+    if(count == 3) 
+        count = 0;
+    else 
+        count++;
+
+    body.style = `background-image: url(${links[count]})`;
 });
